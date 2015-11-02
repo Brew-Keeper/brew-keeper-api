b71f89c1fa9084140365e8d64fcae42ba45f2a52
Kathleen Rauh <krauh2010@gmail.com>
2015-11-02 15:46:14 -0500

    Initial test setup.

--- brewkeeper/api/tests.py
+++ brewkeeper/api/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from nose.tools import *
+
 
 # Create your tests here.
+
+don.pablo = user_id(1)
+
+
+def test_get_user():
+    assert don.pablo.user_id == 1
+    assert don.pablo.user_id != 2
+
+
+def test_get recipe():
+    assert
