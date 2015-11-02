25bd2e41d929b8d48d0e06242c9e44aa90f4bed1
Adam Hartz <ahartz1@gmail.com>
2015-11-02 16:46:15 -0500

    Add basic RecipeViewSet

--- brewkeeper/api/views.py
+++ brewkeeper/api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+# from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, mixins  # , permissions, serializers
+from .models import Recipe, Step, BrewNote
+# from .permissions import IsAPIUser
+from .serializers import RecipeSerializer, RecipeListSerializer, StepSerializer, BrewNoteSerializer
+
 
 # Create your views here.
+
+class RecipeViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin,
+                    mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+    # def get_queryset(self):
+    #     return self.request.user.activity_set.all()
+
+    # def get_serializer_class(self):
+    #     if self.action is not 'list':
+    #         return RecipeSerializer
+    #     else:
+    #         return RecipeListSerializer
