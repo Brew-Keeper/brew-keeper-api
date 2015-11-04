d4ac76934af933e4f7ee4078332c1294a430235f
Kathleen Rauh <krauh2010@gmail.com>
2015-11-04 11:27:50 -0500

    Patch and Post tests functional.

--- brewkeeper/api/tests.py
+++ brewkeeper/api/tests.py
@@ -12,40 +12,38 @@ class RecipeTests(APITestCase):
     def setUp(self):
         Recipe.objects.create(title="The Original", bean_name="Arabica")
 
+
+    def test_read_recipe(self):
+        """
+        Ensure we can read a recipe object.
+        """
+        orig_recipe = Recipe.objects.filter(title='The Original')
+        orig_url = '/api/users/don.pablo/recipes/' + str(orig_recipe[0].pk) + '/'
+        response = self.client.get(orig_url)
+        # self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Recipe.objects.get().orientation, 'Regular')
+
+
     def test_create_recipe(self):
         """
         Ensure we can create a new recipe object.
         """
         url = '/api/users/don.pablo/recipes/'
-        print("Number is: {}".format(Recipe.objects.count()))
         response = self.client.post(url, {"title": "The Impostor"})  # , "bean_name": "Arabica"})
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        print("Number is: {}".format(Recipe.objects.count()))
         self.assertEqual(Recipe.objects.count(), 2)
         posted_recipe = Recipe.objects.filter(title='The Impostor')
-        self.assertEqual(posted_recipe.title, 'The Impostor')
+        self.assertEqual(posted_recipe[0].title, 'The Impostor')
 
 
     def test_patch_recipe(self):
         """
         Ensure we can change a field in a recipe.
         """
-        # url = '/api/users/don.pablo/recipes/1/'
-        # with open('api/testdata/recipe_user_input.json') as data:
-        response = self.client.patch('/api/users/don.pablo/recipes/1/', data={"bean_name": "Robusto"})
-        print(response.content)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(Recipe.objects.count(), 1)
-        self.assertEqual(Recipe.objects.get().bean_name, 'Robusto')
-
-
-    def test_read_recipe(self):
-        """
-        Ensure we can read a recipe object.
-        """
-        url = '/api/users/don.pablo/recipes/1/'
-        # with open('api/testdata/recipe_user_input.json') as data:
-        response = self.client.get(url, orientation="Regular")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(Recipe.objects.count(), 1)
-        self.assertEqual(Recipe.objects.get().orientation, 'Regular')
+        orig_recipe = Recipe.objects.filter(title='The Original')
+        orig_url = '/api/users/don.pablo/recipes/' + str(orig_recipe[0].pk) + '/'
+        response = self.client.patch(orig_url,
+                                     {'bean_name': 'Robusto'},
+                                     format='json')
+        # self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(orig_recipe[0].bean_name, 'Robusto')
