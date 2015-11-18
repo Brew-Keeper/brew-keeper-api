cb87d4718c539ba4ac880d670db30d4da01a1e74
Kathleen Rauh <krauh2010@gmail.com>
2015-11-18 16:33:08 -0500

    Removed django REST swagger.

--- brewkeeper/brewkeeper/settings.py
+++ brewkeeper/brewkeeper/settings.py
@@ -32,7 +32,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_swagger',
     'rest_framework.authtoken',
     'corsheaders',
     'api',
