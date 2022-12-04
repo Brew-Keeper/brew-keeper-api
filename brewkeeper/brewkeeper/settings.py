85479d4a14c93e9934d02615ea595b3936a69e55
Adam J Hartz <ahartz1@gmail.com>
2022-12-04 16:55:54 -0500

    feat: rename MIDDLEWARE_CLASSES to MIDDLEWARE

--- brewkeeper/brewkeeper/settings.py
+++ brewkeeper/brewkeeper/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'api',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
