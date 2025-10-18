db6a80614004103331cf7198d95f1e552c1b7945
Adam J Hartz <ahartz1@gmail.com>
2025-10-18 17:46:05 -0400

    refactor: update serializers to ruff standards, tighten logic

--- brewkeeper/api/serializers.py
+++ brewkeeper/api/serializers.py
@@ -1,205 +1,266 @@
+"""Serializers for the BrewKeeper app."""
+
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers
-from .models import Recipe, Step, BrewNote, PublicRating, PublicComment, UserInfo
+
+from .models import BrewNote, PublicComment, PublicRating, Recipe, Step, UserInfo
 
 
 class StepSerializer(serializers.HyperlinkedModelSerializer):
-    recipe_id = serializers.PrimaryKeyRelatedField(many=False,
-                                                   read_only=True,
-                                                   source='recipe')
+    """Serializer for Step objects."""
 
-    class Meta:
+    recipe_id = serializers.PrimaryKeyRelatedField(
+        many=False, read_only=True, source="recipe"
+    )
+
+    class Meta:  # noqa
         model = Step
-        fields = ('id', 'recipe_id', 'step_number', 'step_title', 'step_body',
-                  'duration', 'water_amount', 'water_units')
+        fields = (
+            "id",
+            "recipe_id",
+            "step_number",
+            "step_title",
+            "step_body",
+            "duration",
+            "water_amount",
+            "water_units",
+        )
 
-    def create(self, validated_data):
-        validated_data['recipe_id'] = self.context['recipe_id']
+    def create(self, validated_data: dict):  # noqa
+        validated_data["recipe_id"] = self.context["recipe_id"]
         try:
-            validated_data['water_units']
-        except:
-            recipe = get_object_or_404(Recipe, pk=self.context['recipe_id'])
-            validated_data['water_units'] = recipe.water_units
-        step = Step.objects.create(**validated_data)
-        return step
+            validated_data["water_units"]
+        except KeyError:
+            recipe = get_object_or_404(Recipe, pk=self.context["recipe_id"])
+            validated_data["water_units"] = recipe.water_units
+        return Step.objects.create(**validated_data)
 
 
 class BrewNoteSerializer(serializers.HyperlinkedModelSerializer):
-    recipe_id = serializers.PrimaryKeyRelatedField(many=False,
-                                                   read_only=True,
-                                                   source='recipe')
+    """Serializer for BrewNote objects."""
 
-    class Meta:
+    recipe_id = serializers.PrimaryKeyRelatedField(
+        many=False, read_only=True, source="recipe"
+    )
+
+    class Meta:  # noqa
         model = BrewNote
-        fields = ('id', 'recipe_id', 'body', 'timestamp')
+        fields = ("id", "recipe_id", "body", "timestamp")
 
-    def create(self, validated_data):
-        validated_data['recipe_id'] = self.context['recipe_id']
-        brew_note = BrewNote.objects.create(**validated_data)
-        return brew_note
+    def create(self, validated_data: dict):  # noqa
+        validated_data["recipe_id"] = self.context["recipe_id"]
+        return BrewNote.objects.create(**validated_data)
 
 
 class PublicRatingSerializer(serializers.HyperlinkedModelSerializer):
-    recipe_id = serializers.PrimaryKeyRelatedField(many=False,
-                                                   read_only=True,
-                                                   source='recipe')
-    username = serializers.PrimaryKeyRelatedField(many=False,
-                                                  read_only=True,
-                                                  source='user.username')
+    """Serializer for PublicRating objects."""
 
-    class Meta:
+    recipe_id = serializers.PrimaryKeyRelatedField(
+        many=False, read_only=True, source="recipe"
+    )
+    username = serializers.PrimaryKeyRelatedField(
+        many=False, read_only=True, source="user.username"
+    )
+
+    class Meta:  # noqa
         model = PublicRating
-        fields = ('id',
-                  'recipe_id',
-                  'username',
-                  'public_rating')
+        fields = ("id", "recipe_id", "username", "public_rating")
 
-    def create(self, validated_data):
-        validated_data['recipe_id'] = self.context['recipe_id']
-        user = get_object_or_404(User, username=self.context['username'])
-        validated_data['user'] = user
-        public_rating = PublicRating.objects.create(**validated_data)
-        return public_rating
+    def create(self, validated_data: dict):  # noqa
+        validated_data["recipe_id"] = self.context["recipe_id"]
+        user = get_object_or_404(User, username=self.context["username"])
+        validated_data["user"] = user
+        return PublicRating.objects.create(**validated_data)
 
 
 class PublicCommentSerializer(serializers.HyperlinkedModelSerializer):
-    recipe_id = serializers.PrimaryKeyRelatedField(many=False,
-                                                   read_only=True,
-                                                   source='recipe')
-    username = serializers.PrimaryKeyRelatedField(many=False,
-                                                  read_only=True,
-                                                  source='user.username')
+    """Serializer for PublicComment objects."""
 
-    class Meta:
+    recipe_id = serializers.PrimaryKeyRelatedField(
+        many=False, read_only=True, source="recipe"
+    )
+    username = serializers.PrimaryKeyRelatedField(
+        many=False, read_only=True, source="user.username"
+    )
+
+    class Meta:  # noqa
         model = PublicComment
-        fields = ('id',
-                  'recipe_id',
-                  'username',
-                  'public_comment',
-                  'comment_timestamp')
+        fields = ("id", "recipe_id", "username", "public_comment", "comment_timestamp")
 
-    def create(self, validated_data):
-        validated_data['recipe_id'] = self.context['recipe_id']
-        user = get_object_or_404(User, username=self.context['username'])
-        validated_data['user'] = user
-        public_comment = PublicComment.objects.create(**validated_data)
-        return public_comment
+    def create(self, validated_data: dict):  # noqa
+        validated_data["recipe_id"] = self.context["recipe_id"]
+        user = get_object_or_404(User, username=self.context["username"])
+        validated_data["user"] = user
+        return PublicComment.objects.create(**validated_data)
 
 
 class RecipeListSerializer(serializers.HyperlinkedModelSerializer):
-    username = serializers.PrimaryKeyRelatedField(many=False,
-                                                  read_only=True,
-                                                  source='user.username')
+    """Serializer for RecipeList objects."""
+
+    username = serializers.PrimaryKeyRelatedField(
+        many=False, read_only=True, source="user.username"
+    )
     steps = StepSerializer(many=True, read_only=True)
 
-    class Meta:
+    class Meta:  # noqa
         model = Recipe
-        fields = ('id', 'title', 'rating', 'bean_name', 'roast', 'brew_count',
-                  'username', 'steps', 'total_duration', 'water_units')
+        fields = (
+            "id",
+            "title",
+            "rating",
+            "bean_name",
+            "roast",
+            "brew_count",
+            "username",
+            "steps",
+            "total_duration",
+            "water_units",
+        )
 
 
 class RecipeDetailSerializer(RecipeListSerializer):
+    """Serializer for RecipeDetail objects."""
+
     brewnotes = BrewNoteSerializer(many=True, read_only=True)
 
-    class Meta:
+    class Meta:  # noqa
         model = Recipe
-        fields = tuple(list(RecipeListSerializer.Meta.fields) +
-                       ['created_on', 'last_brewed_on', 'orientation',
-                        'general_recipe_comment', 'grind', 'total_bean_amount',
-                        'bean_units', 'water_type', 'total_water_amount',
-                        'brewnotes', 'temp'])
+        detail_fields = [
+            "created_on",
+            "last_brewed_on",
+            "orientation",
+            "general_recipe_comment",
+            "grind",
+            "total_bean_amount",
+            "bean_units",
+            "water_type",
+            "total_water_amount",
+            "brewnotes",
+            "temp",
+        ]
+        fields = (*list(RecipeListSerializer.Meta.fields), *detail_fields)
 
-    def create(self, validated_data):
-        '''username in context defined in RecipeViewSet in views.py'''
-        user = get_object_or_404(User, username=self.context['username'])
-        validated_data['user'] = user
-        recipe = Recipe.objects.create(**validated_data)
-        return recipe
+    def create(self, validated_data: dict):
+        """Username in context defined in RecipeViewSet in views.py."""
+        user = get_object_or_404(User, username=self.context["username"])
+        validated_data["user"] = user
+        return Recipe.objects.create(**validated_data)
 
 
 class PublicRecipeListSerializer(serializers.HyperlinkedModelSerializer):
-    username = serializers.PrimaryKeyRelatedField(many=False,
-                                                  read_only=True,
-                                                  source='user.username')
+    """Serializer for PublicRecipeList objects."""
+
+    username = serializers.PrimaryKeyRelatedField(
+        many=False, read_only=True, source="user.username"
+    )
     steps = StepSerializer(many=True, read_only=True)
     public_ratings = serializers.SerializerMethodField()
     combined_rating = serializers.SerializerMethodField()
 
-    class Meta:
+    class Meta:  # noqa
         model = Recipe
-        fields = ('id', 'title', 'rating', 'bean_name', 'roast', 'brew_count',
-                  'username', 'steps', 'total_duration', 'water_units',
-                  'average_rating', 'public_ratings', 'combined_rating',
-                  'shared_by')
+        fields = (
+            "id",
+            "title",
+            "rating",
+            "bean_name",
+            "roast",
+            "brew_count",
+            "username",
+            "steps",
+            "total_duration",
+            "water_units",
+            "average_rating",
+            "public_ratings",
+            "combined_rating",
+            "shared_by",
+        )
 
-    def get_public_ratings(self, obj):
+    def get_public_ratings(self, obj: Recipe):
+        """
+        Get the User's PublicRating for the Recipe in question.
+
+        :param obj: Recipe The Recipe in question
+        :return: dict The serialized ratings
+        """
         try:
-            user = self.context['request'].user
-            public_rating = PublicRating.objects.get(
-                user=user,
-                recipe=obj)
+            user = self.context["request"].user
+            public_rating = PublicRating.objects.get(user=user, recipe=obj)
             serializer = PublicRatingSerializer(public_rating)
             return serializer.data
-        except:
+        except PublicRating.DoesNotExist:
             return {}
 
-    def get_combined_rating(self, obj):
+    def get_combined_rating(self, obj: Recipe):
+        """
+        Get the User's PublicRating for the Recipe in question.
+        """
         try:
-            user = self.context['request'].user
-            user_rating = PublicRating.objects.get(
-                user=user,
-                recipe=obj
-            )
+            user = self.context["request"].user
+            user_rating = PublicRating.objects.get(user=user, recipe=obj)
             return user_rating.public_rating
-        except:
+        except PublicRating.DoesNotExist:
             return obj.average_rating
 
 
 class PublicRecipeDetailSerializer(PublicRecipeListSerializer):
+    """Serializer for PublicRecipeDetail objects."""
+
     brewnotes = BrewNoteSerializer(many=True, read_only=True)
     public_comments = PublicCommentSerializer(many=True, read_only=True)
 
-    class Meta:
+    class Meta:  # noqa
         model = Recipe
-        fields = tuple(list(PublicRecipeListSerializer.Meta.fields) +
-                       ['created_on', 'last_brewed_on', 'orientation',
-                        'general_recipe_comment', 'grind', 'total_bean_amount',
-                        'bean_units', 'water_type', 'total_water_amount',
-                        'brewnotes', 'temp', 'public_comments'])
+        detail_fields = [
+            "created_on",
+            "last_brewed_on",
+            "orientation",
+            "general_recipe_comment",
+            "grind",
+            "total_bean_amount",
+            "bean_units",
+            "water_type",
+            "total_water_amount",
+            "brewnotes",
+            "temp",
+            "public_comments",
+        ]
+        fields = (*list(PublicRecipeListSerializer.Meta.fields), *detail_fields)
 
-    def create(self, validated_data):
-        '''username in context defined in RecipeViewSet in views.py'''
-        user = get_object_or_404(User, username=self.context['username'])
-        validated_data['user'] = user
-        recipe = Recipe.objects.create(**validated_data)
-        return recipe
+    def create(self, validated_data: dict):
+        """Username in context defined in RecipeViewSet in views.py"""
+        user = get_object_or_404(User, username=self.context["username"])
+        validated_data["user"] = user
+        return Recipe.objects.create(**validated_data)
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    """Serializer for User objects."""
 
-    class Meta:
+    class Meta:  # noqa
         model = User
-        fields = ('username', 'password', 'email')
-        extra_kwargs = {'password': {'write_only': True},
-                        'email': {'write_only': True}}
+        fields = ("username", "password", "email")
+        extra_kwargs = {"password": {"write_only": True}, "email": {"write_only": True}}
 
-    def create(self, validated_data):
+    def create(self, validated_data: dict):  # noqa
         user = super().create(validated_data)
-        user.set_password(validated_data['password'])
+        user.set_password(validated_data["password"])
         user.save()
         return user
 
 
 class UserInfoSerializer(serializers.HyperlinkedModelSerializer):
-    username = serializers.PrimaryKeyRelatedField(many=False,
-                                                  read_only=True,
-                                                  source='user.username')
+    """Serializer for UserInfo objects."""
+
+    username = serializers.PrimaryKeyRelatedField(
+        many=False, read_only=True, source="user.username"
+    )
     email = serializers.EmailField(allow_blank=False)
     new_password = serializers.CharField(max_length=None)
     reset_string = serializers.CharField(max_length=27)
 
-    class Meta:
+    class Meta:  # noqa
         model = UserInfo
-        fields = ('id', 'username', 'email', 'new_password', 'reset_string')
-        extra_kwargs = {'new_password': {'write_only': True}}
+        fields = ("id", "username", "email", "new_password", "reset_string")
+        extra_kwargs = {"new_password": {"write_only": True}}
