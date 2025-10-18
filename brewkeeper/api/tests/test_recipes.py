5d56aa6c1f6d80c24611206bf79d19912ebca56d
Adam J Hartz <ahartz1@gmail.com>
2025-10-18 14:34:01 -0400

    test: parameterize test_recipes, ensuring non-creator cannot act

--- brewkeeper/api/tests/test_recipes.py
+++ brewkeeper/api/tests/test_recipes.py
@@ -8,8 +8,36 @@ from api.models import Recipe
 
 from .helpers import authenticate_user
 
+########################################
+# Parametrize Test Case Definitions
+########################################
+
 creator_username = "donpablo"
 other_username = "not_the_creator"
+
+status_200_or_404_test_cases = [
+    # username, expected_status
+    (  # 0 - creator User
+        creator_username,
+        status.HTTP_200_OK,
+    ),
+    (  # 1 - other User
+        other_username,
+        status.HTTP_404_NOT_FOUND,
+    ),
+]
+
+status_204_or_404_test_cases = [
+    # username, expected_status
+    (  # 0 - creator User
+        creator_username,
+        status.HTTP_204_NO_CONTENT,
+    ),
+    (  # 1 - other User
+        other_username,
+        status.HTTP_404_NOT_FOUND,
+    ),
+]
 
 
 recipes_endpoint = f"/api/users/{creator_username}/recipes/"
@@ -24,12 +52,14 @@ class TestRecipes:
             user=user, title="The Original", bean_name="Arabica"
         )
         self.recipe_url = "{}{}/".format(recipes_endpoint, recipe.pk)
-        self.client = authenticate_user()
+
+        User.objects.create(username=other_username, password="password")
 
     @pytest.mark.django_db
     def test_create_recipe(self):
         """Ensure we can create a new Recipe object."""
         # Arrange
+        self.client = authenticate_user(username=creator_username)
         title = "The Impostor"
         with pytest.raises(Recipe.DoesNotExist):
             Recipe.objects.get(title=title)
@@ -42,22 +72,28 @@ class TestRecipes:
         assert Recipe.objects.filter(title=title).count() == 1
 
     @pytest.mark.django_db
-    def test_get_recipe(self):
+    @pytest.mark.parametrize("username, expected_status", status_200_or_404_test_cases)
+    def test_get_recipe(self, username, expected_status):
         """Ensure we can read a Recipe object."""
         # Arrange
+        self.client = authenticate_user(username=username)
         recipe = Recipe.objects.first()
 
         # Act
         response = self.client.get(self.recipe_url)
 
         # Assert
-        assert response.status_code == status.HTTP_200_OK
+        assert response.status_code == expected_status
+        if expected_status != status.HTTP_200_OK:
+            return
         assert response.data["id"] == recipe.pk
 
     @pytest.mark.django_db
-    def test_patch_recipe(self):
+    @pytest.mark.parametrize("username, expected_status", status_200_or_404_test_cases)
+    def test_patch_recipe(self, username, expected_status):
         """Ensure we can change a field in a Recipe object."""
         # Arrange
+        self.client = authenticate_user(username=username)
         recipe = Recipe.objects.get(title="The Original")
         new_bean_name = "Robusto"
         assert recipe.bean_name != new_bean_name
@@ -68,20 +104,26 @@ class TestRecipes:
         )
 
         # Assert
-        assert response.status_code == status.HTTP_200_OK
+        assert response.status_code == expected_status
+        if expected_status != status.HTTP_200_OK:
+            return
         recipe.refresh_from_db()
         assert recipe.bean_name == new_bean_name
 
     @pytest.mark.django_db
-    def test_delete_recipe(self):
+    @pytest.mark.parametrize("username, expected_status", status_204_or_404_test_cases)
+    def test_delete_recipe(self, username, expected_status):
         """Ensure we can delete a Recipe object."""
         # Arrange
+        self.client = authenticate_user(username=username)
         recipe = Recipe.objects.get(title="The Original")
 
         # Act
         response = self.client.delete(self.recipe_url)
 
         # Assert
-        assert response.status_code == status.HTTP_204_NO_CONTENT
+        assert response.status_code == expected_status
+        if expected_status != status.HTTP_204_NO_CONTENT:
+            return
         with pytest.raises(Recipe.DoesNotExist):
             Recipe.objects.get(pk=recipe.pk)
