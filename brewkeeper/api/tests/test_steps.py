abac72a268b6f73438022b16c106b461f3ae1e65
Adam J Hartz <ahartz1@gmail.com>
2025-10-18 14:04:03 -0400

    test: use parametrized to check allowed/disallowed user step actions

--- brewkeeper/api/tests/test_steps.py
+++ brewkeeper/api/tests/test_steps.py
@@ -3,35 +3,80 @@
 from django.contrib.auth.models import User
 import pytest
 from rest_framework import status
-from rest_framework.test import APITestCase
 
 from api.models import Recipe, Step
 
 from .helpers import authenticate_user
 
-steps_endpoint = "/api/users/donpablo/recipes/{}/steps/"
+########################################
+# Parametrize Test Case Definitions
+########################################
+
+creator_username = "donpablo"
+other_username = "not_the_creator"
+
+steps_endpoint = "/api/users/%s/recipes/{}/steps/" % creator_username
+
+status_201_or_404_test_cases = [
+    # username, expected_status
+    (  # 0 - creator User
+        creator_username,
+        status.HTTP_201_CREATED,
+    ),
+    (  # 1 - other User
+        other_username,
+        status.HTTP_404_NOT_FOUND,
+    ),
+]
+
+status_200_or_404_test_cases = [
+    # username, expected_status
+    (  # 0 - creator User
+        creator_username,
+        status.HTTP_200_OK,
+    ),
+    (  # 1 - other User
+        other_username,
+        status.HTTP_404_NOT_FOUND,
+    ),
+]
+
+status_204_or_404_test_cases = [
+    # username, expected_status
+    (  # 0 - creator User
+        creator_username,
+        status.HTTP_204_NO_CONTENT,
+    ),
+    (  # 1 - other User
+        other_username,
+        status.HTTP_404_NOT_FOUND,
+    ),
+]
 
 
 def step_url(step):
     return "{}{}/".format(steps_endpoint.format(step.recipe_id), step.pk)
 
 
-class StepTests(APITestCase):
+class TestSteps:
     """Tests for Step actions."""
 
-    def setUp(self):
-        user = User.objects.create(username="donpablo", password="password")
+    def setup_method(self):
+        user = User.objects.create(username=creator_username, password="password")
         recipe = Recipe.objects.create(
             user=user, title="The Original", bean_name="Arabica"
         )
         Step.objects.create(
             recipe=recipe, duration=10, step_number=1, step_title="Step 1"
         )
-        self.client = authenticate_user()
+        User.objects.create(username=other_username, password="password")
 
-    def test_create_step(self):
+    @pytest.mark.django_db
+    @pytest.mark.parametrize("username, expected_status", status_201_or_404_test_cases)
+    def test_create_step(self, username, expected_status):
         """Ensure we can create a new Step object."""
         # Arrange
+        self.client = authenticate_user(username=username)
         recipe = Recipe.objects.first()
         url = steps_endpoint.format(recipe.pk)
         assert recipe.steps.count() == 1
@@ -47,14 +92,19 @@ class StepTests(APITestCase):
         )
 
         # Assert
-        assert response.status_code == status.HTTP_201_CREATED
+        assert response.status_code == expected_status
+        if expected_status != status.HTTP_201_CREATED:
+            return
         assert recipe.steps.count() == 2
 
-    def test_create_reorders_step(self):
+    @pytest.mark.django_db
+    @pytest.mark.parametrize("username, expected_status", status_201_or_404_test_cases)
+    def test_create_reorders_step(self, username, expected_status):
         """
         Ensure new Step with duplicate step_number reorders the others.
         """
         # Arrange
+        self.client = authenticate_user(username=username)
         recipe = Recipe.objects.first()
         assert recipe.steps.count() == 1
         first_step = recipe.steps.first()
@@ -77,7 +127,9 @@ class StepTests(APITestCase):
         )
 
         # Assert
-        assert response.status_code == status.HTTP_201_CREATED
+        assert response.status_code == expected_status
+        if expected_status != status.HTTP_201_CREATED:
+            return
         first_step.refresh_from_db()
         middle_step.refresh_from_db()
         end_step.refresh_from_db()
@@ -85,42 +137,31 @@ class StepTests(APITestCase):
         assert middle_step.step_number == 3
         assert end_step.step_number == 4
 
-    def test_get_step(self):
+    @pytest.mark.django_db
+    @pytest.mark.parametrize("username, expected_status", status_200_or_404_test_cases)
+    def test_get_step(self, username, expected_status):
         """Ensure we can read a Step object."""
         # Arrange
+        self.client = authenticate_user(username=username)
         step = Step.objects.first()
 
         # Act
         response = self.client.get(step_url(step))
 
         # Assert
-        assert response.status_code == status.HTTP_200_OK
+        assert response.status_code == expected_status
+        if expected_status != status.HTTP_200_OK:
+            return
         assert response.data["id"] == step.pk
 
-    def test_patch_step(self):
-        """Ensure we can change a field in a Step object."""
-        # Arrange
-        step = Step.objects.first()
-        new_duration = 18
-        assert step.duration != new_duration
-
-        # Act
-        response = self.client.patch(
-            step_url(step),
-            {"duration": new_duration, "step_number": 1, "step_title": "Step 1"},
-            format="json",
-        )
-
-        # Assert
-        assert response.status_code == status.HTTP_200_OK
-        step.refresh_from_db()
-        assert step.duration == new_duration
-
-    def test_patch_reorders_step(self):
+    @pytest.mark.django_db
+    @pytest.mark.parametrize("username, expected_status", status_200_or_404_test_cases)
+    def test_patch_reorders_step(self, username, expected_status):
         """
         Ensure Steps reorder when patched step_number overlaps existing.
         """
         # Arrange
+        self.client = authenticate_user(username=username)
         recipe = Recipe.objects.first()
         assert recipe.steps.count() == 1
         first_step = recipe.steps.first()
@@ -140,7 +181,9 @@ class StepTests(APITestCase):
         )
 
         # Assert
-        assert response.status_code == status.HTTP_200_OK
+        assert response.status_code == expected_status
+        if expected_status != status.HTTP_200_OK:
+            return
         first_step.refresh_from_db()
         middle_step.refresh_from_db()
         end_step.refresh_from_db()
@@ -148,22 +191,30 @@ class StepTests(APITestCase):
         assert middle_step.step_number == 1
         assert end_step.step_number == 3
 
-    def test_delete_step(self):
+    @pytest.mark.django_db
+    @pytest.mark.parametrize("username, expected_status", status_204_or_404_test_cases)
+    def test_delete_step(self, username, expected_status):
         """Ensure we can delete a Step object."""
         # Arrange
+        self.client = authenticate_user(username=username)
         step = Step.objects.first()
 
         # Act
         response = self.client.delete(step_url(step))
 
         # Assert
-        assert response.status_code == status.HTTP_204_NO_CONTENT
+        assert response.status_code == expected_status
+        if expected_status != status.HTTP_204_NO_CONTENT:
+            return
         with pytest.raises(Step.DoesNotExist):
             Step.objects.get(pk=step.pk)
 
-    def test_delete_middle_step(self):
+    @pytest.mark.django_db
+    @pytest.mark.parametrize("username, expected_status", status_204_or_404_test_cases)
+    def test_delete_middle_step(self, username, expected_status):
         """Ensure deleting a middle Step properly reorders remaining."""
         # Arrange
+        self.client = authenticate_user(username=username)
         recipe = Recipe.objects.first()
         middle_step = Step.objects.create(
             recipe=recipe, duration=10, step_number=2, step_title="Step 2"
@@ -176,6 +227,8 @@ class StepTests(APITestCase):
         response = self.client.delete(step_url(middle_step))
 
         # Assert
-        assert response.status_code == status.HTTP_204_NO_CONTENT
+        assert response.status_code == expected_status
+        if expected_status != status.HTTP_204_NO_CONTENT:
+            return
         end_step.refresh_from_db()
         assert end_step.step_number == 2
